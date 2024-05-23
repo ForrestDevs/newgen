a80f3db11b14c40631e25345bed3670345eb2695
Luke Gannon <luke.gannon@me.com>
2024-05-23 18:40:55 -0400

    Removed Stripe API

--- src/lib/stripe/subscription.ts
+++ src/lib/stripe/subscription.ts
@@ -1,6 +1,6 @@
 import { storeSubscriptionPlans } from "@/config/subscriptions";
 import { db } from "@/lib/db/index";
-import { subscriptions } from "@/lib/db/schema/subscriptions";
+// import { subscriptions } from "@/lib/db/schema/subscriptions";
 import { eq } from "drizzle-orm";
 import { stripe } from "@/lib/stripe/index";
 import { validateRequest } from "@/lib/auth";
@@ -12,50 +12,50 @@ export async function getUserSubscriptionPlan() {
     throw new Error("User not found.");
   }
 
-  const [ subscription ] = await db
-    .select()
-    .from(subscriptions)
-    .where(eq(subscriptions.userId, user.id));
+  // const [ subscription ] = await db
+  //   .select()
+  //   .from(subscriptions)
+  //   .where(eq(subscriptions.userId, user.id));
 
-  if (!subscription)
-    return {
-      id: undefined,
-      name: undefined,
-      description: undefined,
-      stripePriceId: undefined,
-      price: undefined,
-      stripeSubscriptionId: null,
-      stripeCurrentPeriodEnd: null,
-      stripeCustomerId: null,
-      isSubscribed: false,
-      isCanceled: false,
-    };
+  // if (!subscription)
+  //   return {
+  //     id: undefined,
+  //     name: undefined,
+  //     description: undefined,
+  //     stripePriceId: undefined,
+  //     price: undefined,
+  //     stripeSubscriptionId: null,
+  //     stripeCurrentPeriodEnd: null,
+  //     stripeCustomerId: null,
+  //     isSubscribed: false,
+  //     isCanceled: false,
+  //   };
 
-  const isSubscribed =
-    subscription.stripePriceId &&
-    subscription.stripeCurrentPeriodEnd &&
-    subscription.stripeCurrentPeriodEnd.getTime() + 86_400_000 > Date.now();
+  // const isSubscribed =
+  //   subscription.stripePriceId &&
+  //   subscription.stripeCurrentPeriodEnd &&
+  //   subscription.stripeCurrentPeriodEnd.getTime() + 86_400_000 > Date.now();
 
-  const plan = isSubscribed
-    ? storeSubscriptionPlans.find(
-        (plan) => plan.stripePriceId === subscription.stripePriceId
-      )
-    : null;
+  // const plan = isSubscribed
+  //   ? storeSubscriptionPlans.find(
+  //       (plan) => plan.stripePriceId === subscription.stripePriceId
+  //     )
+  //   : null;
 
-  let isCanceled = false;
-  if (isSubscribed && subscription.stripeSubscriptionId) {
-    const stripePlan = await stripe.subscriptions.retrieve(
-      subscription.stripeSubscriptionId
-    );
-    isCanceled = stripePlan.cancel_at_period_end;
-  }
+  // let isCanceled = false;
+  // if (isSubscribed && subscription.stripeSubscriptionId) {
+  //   const stripePlan = await stripe.subscriptions.retrieve(
+  //     subscription.stripeSubscriptionId
+  //   );
+  //   isCanceled = stripePlan.cancel_at_period_end;
+  // }
 
-  return {
-    ...plan,
-    stripeSubscriptionId: subscription.stripeSubscriptionId,
-    stripeCurrentPeriodEnd: subscription.stripeCurrentPeriodEnd,
-    stripeCustomerId: subscription.stripeCustomerId,
-    isSubscribed,
-    isCanceled,
-  };
+  // return {
+  //   ...plan,
+  //   stripeSubscriptionId: subscription.stripeSubscriptionId,
+  //   stripeCurrentPeriodEnd: subscription.stripeCurrentPeriodEnd,
+  //   stripeCustomerId: subscription.stripeCustomerId,
+  //   isSubscribed,
+  //   isCanceled,
+  // };
 }
