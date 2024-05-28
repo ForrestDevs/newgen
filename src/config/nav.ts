13120f46061b5fc29d0f2ad0cb95675b2e9c74b4
Luke Gannon <luke.gannon@me.com>
2024-05-28 00:30:05 -0400

    Updated imports

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,12 +1,12 @@
-import { SidebarLink } from "@/components/SidebarItems";
+import { SidebarLinkProps } from "@/components/layout/app/app-sidebar";
 import { Cog, Globe, HomeIcon } from "lucide-react";
 
 type AdditionalLinks = {
   title: string;
-  links: SidebarLink[];
+  links: SidebarLinkProps[];
 };
 
-export const defaultLinks: SidebarLink[] = [
+export const defaultLinks: SidebarLinkProps[] = [
   { href: "/dashboard", title: "Home", icon: HomeIcon },
   { href: "/account", title: "Account", icon: Cog },
   { href: "/settings", title: "Settings", icon: Cog },
